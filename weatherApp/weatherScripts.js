321ab3d35f2925a1aff274bc61b46b260843c6c9
roshansharma <roshan.s4d@gmail.com>
2022-11-18 09:31:24 +0530

    Baby Weather App

---
+++ weatherApp/weatherScripts.js
@@ -0,0 +1,7 @@
+//basic example to demonstrate use of condititonals
+let weather = prompt("How's the weather outside")
+//conditionals
+if (weather == 'rainy')
+    alert('Take an umbrella with you')
+else
+    alert('Wear your sunglasses')
